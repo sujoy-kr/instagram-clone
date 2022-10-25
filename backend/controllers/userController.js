f4178a857d37196c10c97c2b1d91c7fa774052a8
sujoy <sujoykr8@gmail.com>
2022-10-26 01:02:33 +0600

    add login route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,30 +1,54 @@
-const { user: User } = require("../models");
+const {user: User} = require('../models')
 
 const getAllUser = async (req, res) => {
-  try {
-    const users = await User.findAll();
-    res.status(200).json(users);
-  } catch (e) {
-    res.status(500).json(e.message);
-  }
-};
+    try {
+        const users = await User.findAll()
+        res.status(200).json(users)
+    } catch (e) {
+        res.status(500).json(e.message)
+    }
+}
 
 const postUser = async (req, res) => {
-  try {
-    const { username, password, name, email } = req.body;
-    const user = await User.create({
-      username: username,
-      password: password,
-      name: name,
-      email: email,
-    });
-    res.status(201).json(user);
-  } catch (e) {
-    res.status(500).json(e.message);
-  }
-};
+    try {
+        const {username, password, name, email} = req.body
+        const user = await User.create({
+            username: username,
+            password: password,
+            name: name,
+            email: email,
+        })
+        res.status(201).json(user)
+    } catch (e) {
+        res.status(500).json(e.message)
+    }
+}
+
+const loginUser = async (req, res) => {
+    const {username, password} = req.body
+    if (username && password) {
+        try {
+            const user = await User.findOne({
+                where: {
+                    username,
+                    password
+                }
+            })
+            if (user) {
+                res.json(user)
+            } else {
+                res.status(404).json({message: 'user not found'})
+            }
+        } catch (e) {
+            res.json({message: e.message})
+        }
+    } else {
+        res.json({message: 'Username or password can\'t be empty'})
+    }
+}
 
 module.exports = {
-  getAllUser,
-  postUser,
-};
+    getAllUser,
+    postUser,
+    loginUser
+}
--- backend/routes/api/user.js
+++ backend/routes/api/user.js
@@ -1,7 +1,8 @@
-const router = require("express").Router();
-const userController = require("../../controllers/userController");
+const router = require('express').Router()
+const userController = require('../../controllers/userController')
 
-router.get("/", userController.getAllUser);
-router.post("/", userController.postUser);
+router.get('/', userController.getAllUser)
+router.post('/', userController.postUser)
+router.post('/login', userController.loginUser)
 
-module.exports = router;
+module.exports = router
