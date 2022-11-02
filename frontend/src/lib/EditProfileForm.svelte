d93e3df5fc511440bf663255ba971a979938124d
sujoy <sujoykr8@gmail.com>
2022-11-03 00:56:06 +0600

    add edit profile form

---
+++ frontend/src/lib/EditProfileForm.svelte
@@ -0,0 +1,60 @@
+<div class="p-4 ">
+    <form class="bg-white rounded-md max-w-fit p-4 mx-auto my-4"
+          enctype="multipart/form-data">
+        <div class="flex flex-col">
+            <div class="text-right w-full mb-4">
+                <label for="name">Name: </label>
+                <input id="name" class="text-sm focus:outline-none rounded-md bg-slate-100 p-2" type="text"
+                       name="name"/>
+            </div>
+            <div class="text-right w-full mb-4">
+                <label for="bio">Bio: </label>
+                <input id="bio" class="text-sm focus:outline-none rounded-md bg-slate-100 p-2" type="text"
+                       name="bio"/>
+            </div>
+            <div class="text-right w-full mb-4">
+                <label for="profilePic">Profile Picture: </label>
+                <input id="profilePic" class="text-sm focus:outline-none rounded-md bg-slate-100 p-2" type="file"
+                       name="image"/>
+            </div>
+        </div>
+        <div class="text-center mt-4 mb-2">
+            <button class="px-8 py-2 text-sm bg-blue-500 tracking-wide rounded-md text-white font-semibold"
+                    type="submit">Save
+            </button>
+        </div>
+    </form>
+</div>
+
+
+<style>
+    form {
+        box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+    }
+
+    form input {
+        width: 250px;
+        height: 40px;
+        margin-left: 10px;
+    }
+
+    .text-right {
+        text-align: right;
+    }
+
+
+    @media only screen and (max-width: 500px) {
+        .text-right {
+            text-align: center;
+        }
+
+        form label {
+            display: block;
+        }
+
+        form input {
+            width: 100%;
+            margin-left: 0;
+        }
+    }
+</style>
---
+++ frontend/src/routes/editprofile/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import Navbar from '$lib/Navbar.svelte';
+    import FooterNav from '$lib/FooterNav.svelte';
+    import EditProfileForm from '$lib/EditProfileForm.svelte';
+</script>
+
+<Navbar/>
+<EditProfileForm/>
+<FooterNav/>
